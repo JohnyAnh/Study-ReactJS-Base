a31c42f40d3505154d59b7b0fb7e53c778aae899
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-06-22 19:01:39 +0700

    React Dev Tools - Công Cụ Hữu Ích Của DEV

--- reactbasic/src/views/Example/MyComponent.js
+++ reactbasic/src/views/Example/MyComponent.js
@@ -22,18 +22,13 @@ class MyComponent extends React.Component {
         let name = 'Johny';
         return (
             <>
-                <div className='example'>
+                <div className='first'>
                     {console.log('Hello my name is:', name)}
                     <input value={this.state.name} type="text"
                         onChange={(event) => this.handleOnchangeName(event)} />
                     My name is {this.state.name}<br />
-
-                </div>
-                <div className='fist'>
-                    {console.log('Hello my name is:', name)}
-                    Hello my component React JS. My name is {this.state.name}<br />
                     {/* Hello my component React JS. My name is {this.state['name']}<br /> */}
-                    Hello my component React JS. My name is {name}
+
                 </div>
                 <div className='second'>
                     I'm {this.state.age} year old <br />
