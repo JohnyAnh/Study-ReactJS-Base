c170257fc491d4c0954aaa82a8fbfe5a34ae7cd4
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-06-22 17:09:30 +0700

    State - Kiểm Soát Trạng Thái Ứng Dụng React.JS

--- reactbasic/src/views/Example/MyComponent.js
+++ reactbasic/src/views/Example/MyComponent.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    // obj: key : value
+    state = {
+        name: 'Johny Vu',
+        age: 26
+    }
+    /**
+     * JSX => return block code
+     * fragment
+     */
+
+    //Example
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
+        let name = 'Johny';
         return (
-            <div>Hello my component React JS</div>
+            <>
+                <div className='example'>
+                    {console.log('Hello my name is:', name)}
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnchangeName(event)} />
+                    My name is {this.state.name}<br />
+
+                </div>
+                <div className='fist'>
+                    {console.log('Hello my name is:', name)}
+                    Hello my component React JS. My name is {this.state.name}<br />
+                    {/* Hello my component React JS. My name is {this.state['name']}<br /> */}
+                    Hello my component React JS. My name is {name}
+                </div>
+                <div className='second'>
+                    I'm {this.state.age} year old <br />
+                    I'm {this.state['age']} year old
+                </div>
+            </>
         )
     }
 }
